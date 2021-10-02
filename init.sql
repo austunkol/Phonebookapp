926fd8af37872d08c778f177933d10c6265819f2
Ali Ustunkol <64160260+austunkol@users.noreply.github.com>
2021-10-02 09:27:34 -0400

    phonebookapp

---
+++ init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE phonebook_db.phonebook(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    number VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO phonebook_db.phonebook (name, number)
+    VALUES
+        ("Ali", "1234567890"),
+        ("Taco", "67854"),
+        ("Altobelli", "876543554");
